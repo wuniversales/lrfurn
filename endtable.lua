2fd4403eae715b0568a1ede16c4dc38e272e7a28
wuniversales <48658161+wuniversales@users.noreply.github.com>
2020-03-26 22:07:57 +0100

    Update endtable.lua

--- endtable.lua
+++ endtable.lua
@@ -7,6 +7,10 @@ else
   S = function ( s ) return s end
 end
 
+if minetest.get_modpath("mcl_sounds") then
+    default=mcl_sounds
+end
+
 minetest.register_node("lrfurn:endtable", {
 	description = S("End Table"),
 	drawtype = "nodebox",
@@ -37,33 +41,61 @@ minetest.register_node("lrfurn:endtable", {
 	},
 })
 
-minetest.register_craft({
-	output = "lrfurn:endtable",
-	recipe = {
-		{"", "", "", },
-		{"stairs:slab_wood", "stairs:slab_wood", "", },
-		{"default:stick", "default:stick", "", }
-	}
-})
+if minetest.get_modpath("mcl_core") then
+    minetest.register_craft({
+        output = "lrfurn:endtable",
+        recipe = {
+            {"", "", "", },
+            {"mcl_stairs:slab_wood", "mcl_stairs:slab_wood", "", },
+            {"mcl_core:stick", "mcl_core:stick", "", }
+        }
+    })
 
-minetest.register_craft({
-	output = "lrfurn:endtable",
-	recipe = {
-		{"", "", "", },
-		{"moreblocks:slab_wood", "moreblocks:slab_wood", "", },
-		{"default:stick", "default:stick", "", }
-	}
-})
+    minetest.register_craft({
+        output = "lrfurn:endtable",
+        recipe = {
+            {"", "", "", },
+            {"mcl_core:stick", "mcl_core:stick", "", },
+            {"mcl_core:stick", "mcl_core:stick", "", }
+        }
+    })
 
-minetest.register_craft({
-	output = "lrfurn:endtable",
-	recipe = {
-		{"", "", "", },
-		{"group:wood_slab", "group:wood_slab", "", },
-		{"default:stick", "default:stick", "", }
-	}
-})
+    minetest.register_craft({
+        output = "lrfurn:endtable",
+        recipe = {
+            {"", "", "", },
+            {"group:wood_slab", "group:wood_slab", "", },
+            {"mcl_core:stick", "mcl_core:stick", "", }
+        }
+    })
+else
+    minetest.register_craft({
+        output = "lrfurn:endtable",
+        recipe = {
+            {"", "", "", },
+            {"stairs:slab_wood", "stairs:slab_wood", "", },
+            {"default:stick", "default:stick", "", }
+        }
+    })
 
+    minetest.register_craft({
+        output = "lrfurn:endtable",
+        recipe = {
+            {"", "", "", },
+            {"moreblocks:slab_wood", "moreblocks:slab_wood", "", },
+            {"default:stick", "default:stick", "", }
+        }
+    })
+
+    minetest.register_craft({
+        output = "lrfurn:endtable",
+        recipe = {
+            {"", "", "", },
+            {"group:wood_slab", "group:wood_slab", "", },
+            {"default:stick", "default:stick", "", }
+        }
+    })
+end
 if minetest.setting_get("log_mods") then
 	minetest.log("action", "endtable loaded")
 end
